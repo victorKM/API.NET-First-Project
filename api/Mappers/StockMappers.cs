d3bfbcec0c7b7e2b97c6e0929ecb10a4ca677c11
Victor Kikuti <victor.kikuti@gmail.com>
2024-07-08 11:49:59 -0300

    add DTOS

---
+++ api/Mappers/StockMappers.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using api.Models;
+using api.Dtos.Stock;
+
+namespace api.Mappers
+{
+    public static class StockMappers
+    {
+        public static StockDto ToStockDto(this Stock stockModel)
+        {
+            return new StockDto
+            {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDiv = stockModel.LastDiv,
+                Industry = stockModel.Industry,
+                MarketCap = stockModel.MarketCap
+            };
+        }
+    }
+}
