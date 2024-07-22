29a7d75926600aa1300950f7f362a8c2a9a0f9cf
Victor Kikuti <victor.kikuti@gmail.com>
2024-07-22 12:00:07 -0300

    post comments

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faf8725c23fe88fef2c64629a744e3b873e1386d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08b636ea1a333381ef771365d98459d2f905691a")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
