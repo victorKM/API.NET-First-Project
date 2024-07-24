83a2e6222005a0653e46436bd47453aad918f2b0
Victor Kikuti <victor.kikuti@gmail.com>
2024-07-24 14:50:51 -0300

    sorting

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e42281a334e345c48d2b519534e45b867684c615")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a1c98f809e5ad6afc4b19aeeac819d4e33ef1c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
