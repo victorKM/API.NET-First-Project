891a99ae6d107f08198776e8cc4dd45a7296796f
Victor Kikuti <victor.kikuti@gmail.com>
2024-07-30 16:59:02 -0300

    add the delete portfolio

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a11d11e26b2876871698bb414c06978e05096e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24b5afcc099c23f4fe612eda75685a24b69a54de")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
